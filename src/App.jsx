3815a125a73f1a05b052993a23a0cbc72e9cc1cc
ahadu <ahadu4321@gmail.com>
2024-08-03 17:53:26 +0300

    ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,35 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [coins, setCoins] = useState(100)
   const [st,setSt]=useState('')
   const increment=()=>{
     setCount(count+1);
-    setSt('scale')
-    console.log(count)
+    setSt('scale');
+    coins > 0 ? setCoins(coins-1) : refill();
   }
  
+  const refill = ()=>{
+    setTimeout(()=>{setCoins(100)});
+  }
   return (
     <>
     <div className="nav">
     <span className='topT'>Balance</span> 
-     <span className='float'> {count} coins</span> 
+     <span className='float golden'> {count} 🪙</span> 
     </div>
-      <div className='row'>     
+    <h1 className='float white'>+1</h1>
+      <div className='row'>    
       <div className={'circle '+ st } onClick={increment}>
     
       </div>
       </div>
+
+      <div className="flex">
+      <input type="range" value={coins} />
+      <b className='white'>energy level {coins} coins</b>
+      </div>
+
       <div className='footer golden'>
         <span className='btn'>🏠 Home</span>
         <span className='btn'>💰 Earn</span>

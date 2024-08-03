c1501118d4171a750778f55bf9f7c054366eccf9
ahadu <ahadu4321@gmail.com>
2024-08-03 16:49:30 +0300

    ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,33 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [st,setSt]=useState('')
   const increment=()=>{
     setCount(count+1);
+    setSt('scale')
     console.log(count)
   }
-  useEffect(()=>{
-    increment();
-  },[]);
+ 
   return (
     <>
     <div className="nav">
     <span className='topT'>Balance</span> 
-     <span className='float'> {count}</span> 
+     <span className='float'> {count} coins</span> 
     </div>
       <div className='row'>     
-      <div className="circle" onClick={increment}>
+      <div className={'circle '+ st } onClick={increment}>
     
       </div>
       </div>
-      <h1>Ahadu Coin</h1>
+      <h3 className='golden'>Thor Coin</h3>
       <div className="card">
        
-        <p>
+        <p className='golden'>
        tap and earn rewards
         </p>
+      </div>
+      <div className='footer golden'>
+        <span>Home</span>Earn<span></span>Reward<span>Wallet</span>
       </div>
     </>
   )
